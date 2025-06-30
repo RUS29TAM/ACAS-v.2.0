9bc9a074be642ae0b09b09df1bf410cc1397e3b7
React <rusram.nikonchuk@yandex.ru>
2025-06-30 14:28:40 +0300

    chore: configure models

--- src/models/client.ts
+++ src/models/client.ts
@@ -3,6 +3,9 @@ import { Client as PrismaClient } from '@prisma/client';
 export interface Client extends PrismaClient {
     // Дополнительные поля, если нужны
     centerName?: string;
+    Center?: {
+        name: string;
+    };
 }
 
 // Тип для создания/обновления клиента (без id и timestamps)
